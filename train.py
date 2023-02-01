026b19b5750fbe47c2b41a9677327af275ef488a
sungjun <junjun607@gmail.com>
2023-02-01 17:34:34 +0900

    s2v9 embedding_size=128

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ def train():
     # hyperparameters
     classes = 1211
     learning_rate = 0.001
-    embedding_size = 256
+    embedding_size = 128
     n_mels = 40
     epochs = 150
     batch_size = 512
@@ -48,7 +48,7 @@ def train():
 
     # wandb 설정
     os.system('wandb login be65d6ddace6bf4e2441a82af03c144eb85bbe65')
-    wandb.init(project='dvector-original-s2v8', entity='dvector')
+    wandb.init(project='dvector-original-s2v9', entity='dvector')
     wandb.config = {
         "learning_rate" : learning_rate,
         "epochs" : epochs,
